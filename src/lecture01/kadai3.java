1d7668e4ebdd2482817a90f87b1785ababfa514c
[moriprog] <[b2212410@photon.chitose.ac.jp>
2022-11-05 14:42:06 +0900

    kadai3

--- src/lecture01/kadai3.java
+++ src/lecture01/kadai3.java
@@ -1,4 +1,23 @@
 package lecture01;
 
+import java.util.Scanner;
+
 public class kadai3 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("年齢を入力");
+        int age=input.nextInt();
+
+        if(age>=20){
+            System.out.println("I am " + age + " years old so I can drink liquor.");
+        }
+        else if (age<20){
+            System.out.println("I am " + age + " years old so I cannot drink liquor.");
+
+        }
+    }
+
+
+
 }
