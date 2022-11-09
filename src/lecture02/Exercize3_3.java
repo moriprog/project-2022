4f169db3b322133174db189a5d18a3959dce9280
[moriprog] <[b2212410@photon.chitose.ac.jp>
2022-11-09 12:04:51 +0900

    車

--- src/lecture02/Exercize3_3.java
+++ src/lecture02/Exercize3_3.java
@@ -4,7 +4,7 @@ public class Exercize3_3 {
     public static void main(String[] args) {
         Tire[] tires=new Tire[4];
         for(int i=0;i<4;i++){
-            tires[i]=new Tire(64);
+            tires[i]=new Tire(65);
         }
         Engine engine=new Engine(4000);
         Car car=new Car(tires,engine);
