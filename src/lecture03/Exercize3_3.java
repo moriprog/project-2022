2af9e7dd42a19ca5454a75a296c447e7a56f9b8f
[moriprog] <[b2212410@photon.chitose.ac.jp>
2022-11-10 22:16:19 +0900

    計算

--- src/lecture03/Exercize3_3.java
+++ src/lecture03/Exercize3_3.java
@@ -1,2 +1,25 @@
-package lecture03;public class Exercize3_3 {
+package lecture03;
+
+import java.util.Scanner;
+import java.util.SortedMap;
+
+
+public class Exercize3_3 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("xを入力");
+        String x=input.nextLine();
+        System.out.println("yを入力");
+        String y=input.nextLine();
+
+        int xi=Integer.parseInt(x);
+        int yi=Integer.parseInt(y);
+
+        int sum=xi+yi;
+
+        System.out.println("和"+sum);
+    }
+
+
+
 }
