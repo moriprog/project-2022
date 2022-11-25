50e0733a2e7af6a04fe0d56374d13a14963471a9
[moriprog] <[b2212410@photon.chitose.ac.jp>
2022-11-25 14:52:45 +0900

    ATM

--- src/lecture04/Exercize4_2.java
+++ src/lecture04/Exercize4_2.java
@@ -1,6 +1,5 @@
 package lecture04;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Exercize4_2 {
@@ -8,7 +7,7 @@ public class Exercize4_2 {
         Scanner input=new Scanner(System.in);
         System.out.print("名前;");
         String name=input.nextLine();
-        System.out.print("口座番号");
+        System.out.print("口座番号;");
         String number=input.nextLine();
 
         Account account=new Account(name,number);
@@ -18,9 +17,22 @@ public class Exercize4_2 {
         atm.registerAccount(name,number);
         atm.existAccount(name,number);
 
-        atm.deposit(number,1000);
-        atm.withdraw(number,2000);
-        atm.withdraw(number,500);
+        while (true){
+            System.out.print("入金したいなら1,引出したいなら2,終了させたいなら0を入力:");
+            int n=input.nextInt();
+            if(n==1){
+                System.out.print("入金したい金額を入力:");
+                long money= input.nextLong();
+                atm.deposit(number,money);
+            } else if (n==2) {
+                System.out.println("引き出したい金額を入力");
+                long money=input.nextLong();
+                atm.withdraw(number,money);
+            } else {
+                System.out.println("終了しました");
+                break;
+            }
+        }
 
 
 
