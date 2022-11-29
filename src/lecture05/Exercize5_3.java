09b6dc20a19c324145e39422339e91cf7be4be1d
[moriprog] <[b2212410@photon.chitose.ac.jp>
2022-11-29 15:05:56 +0900

    飛んだよ

---
+++ src/lecture05/Exercize5_3.java
@@ -0,0 +1,10 @@
+package lecture05;
+
+public class Exercize5_3 {
+    public static void main(String[] args) {
+        Butterfly tyou=new Butterfly();
+        Insect insect=tyou;
+        insect.setName();
+        insect.move();
+    }
+}
