20988836df3010d37cf8bea4d4f4ea04ef05e3d9
[moriprog] <[b2212410@photon.chitose.ac.jp>
2022-11-29 16:00:59 +0900

    アゲハ

---
+++ src/lecture05/Exercize5_6.java
@@ -0,0 +1,10 @@
+package lecture05;
+
+public class Exercize5_6 {
+    public static void main(String[] args) {
+        SwallowtailButterfly ageha=new SwallowtailButterfly();
+        ageha.setName();
+        ageha.move();
+    }
+
+}
