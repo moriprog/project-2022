3ea060023c4aa72a7d7a3f9383f1859929c8c6f7
[moriprog] <[b2212410@photon.chitose.ac.jp>
2022-11-29 15:26:56 +0900

    バッタ

--- src/lecture05/Exercize5_4.java
+++ src/lecture05/Exercize5_4.java
@@ -1,2 +1,10 @@
-package lecture05;public class Exercize5_4 {
+package lecture05;
+
+public class Exercize5_4 {
+    public static void main(String[] args) {
+        Locust batta=new Locust();
+        batta.setName();
+        batta.move();
+
+    }
 }
