bdbdf5ca8e6b60bb5e94c15a7128b57b8771e59d
[moriprog] <[b2212410@photon.chitose.ac.jp>
2022-11-29 15:56:33 +0900

    跳んだよ

---
+++ src/lecture05/Exercize5_5.java
@@ -0,0 +1,11 @@
+package lecture05;
+
+public class Exercize5_5 {
+    public static void main(String[] args) {
+        Locust batta=new Locust();
+        Insect insect=batta;
+        insect.setName();
+        insect.move();
+    }
+
+}
