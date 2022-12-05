2b34616b3960c4ce205aedaf08b1434d6df5ffcd
[moriprog] <[b2212410@photon.chitose.ac.jp>
2022-12-05 22:22:57 +0900

    さいころ

---
+++ src/lecture06/Exercize6_2.java
@@ -0,0 +1,43 @@
+package lecture06;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercize6_2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        System.out.println("サイコロを5個振りました");
+
+        for(int i = 0;i < 5;i++){
+            list.add(random.nextInt(6) + 1);
+        }
+
+
+        try{
+
+            System.out.println("何番目のサイコロの値を確認しますか？");
+            int a = input.nextInt();
+            int b = a - 1;
+            System.out.println(list.get(b));
+        }
+        catch(InputMismatchException e){
+            System.out.println("整数以外の値が入力されました");
+            System.out.println("プログラムを終了します");
+
+
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("ArrayListの範囲外アクセスを確認しました");
+            System.out.println("プログラムを終了します");
+
+        }
+
+
+
+    }
+}
